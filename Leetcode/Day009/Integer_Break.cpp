f0e7221ae2458cf101bf145cdf749be9604aadea
Sujal <sujalahrodia@gmail.com>
2019-03-05 21:18:31 -0500

    Update Integer_Break.cpp

--- Leetcode/Day009/Integer_Break.cpp
+++ Leetcode/Day009/Integer_Break.cpp
@@ -21,3 +21,30 @@ public:
         return dp[n];
     }
 };
+
+// If we start with n>2
+
+class Solution {
+public:
+    int integerBreak(int n) 
+    {
+        int dp[n+1]={1};
+        dp[0]=0, dp[1]=1;
+        
+        for(int i=2; i<=n; i++)
+        {
+            int temp=0;
+            for(int j=1; j<i; j++)
+            {
+                int prod=dp[j]*(i-j);
+                if(prod>temp)
+                    temp=prod;
+            }
+            if(i<n)
+                temp=max(temp,i);
+            dp[i]=temp;
+        }
+        
+        return dp[n];
+    }
+};
