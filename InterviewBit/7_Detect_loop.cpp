cd0492e1e5ea428809a074c8d71ba4b826a91f52
Sujal <sujalahrodia@gmail.com>
2019-03-22 18:14:36 -0400

    Create 7_Detect_loop.cpp

---
+++ InterviewBit/7_Detect_loop.cpp
@@ -0,0 +1,14 @@
+int detectloop(Node *head)
+{
+    Node *slow=head;
+    Node *fast=head->next;
+    
+    while(slow && fast && fast->next)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast)
+            return 1;
+    }
+    return 0;
+}
