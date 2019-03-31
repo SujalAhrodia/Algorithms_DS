4bfb0308e22bedd5fb1c99a189381eca62b5dcde
Sujal <sujalahrodia@gmail.com>
2019-03-30 22:58:48 -0400

    Create Jewel_Stone.cpp

---
+++ Leetcode/Day013/Jewel_Stone.cpp
@@ -0,0 +1,42 @@
+//Naive approach
+
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) 
+    {
+        int ans=0;
+        
+        for(int i=0; i<S.length(); i++)
+        {
+            for(int j=0; j<J.length(); j++)
+            {
+                if(S[i]==J[j])
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+
+// Hash Table(in this case it costs more memory)
+
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) 
+    {
+        int ans=0;
+        
+        unordered_set<char> table;
+        
+        for(int j=0; j<J.length(); j++)
+            table.insert(J[j]);
+        
+        for(int i=0; i<S.length(); i++)
+        {
+            if(table.find(S[i])!=table.end())
+                ans++;
+        }
+        return ans;
+    }
+};
