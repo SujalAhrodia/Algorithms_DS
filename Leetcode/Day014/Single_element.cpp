082361ca5da7b148f5b2133ba823ba74e5a58450
Sujal <sujalahrodia@gmail.com>
2019-04-02 20:00:05 -0400

    Update Single_element.cpp

--- Leetcode/Day014/Single_element.cpp
+++ Leetcode/Day014/Single_element.cpp
@@ -31,3 +31,35 @@ public:
         return ans;
     }
 };
+
+//Binary Search: 
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int start=0, end=n-1;
+        
+        while(start<end)
+        {
+            int mid = start + (end-start)/2;
+            
+            if(mid%2==0)
+            {
+                if(nums[mid-1]==nums[mid])
+                    end=mid-2;
+                else
+                    start=mid+2;
+            }
+            else
+            {
+                if(nums[mid-1]==nums[mid])
+                    start=mid+1;
+                else
+                    end=mid-1;
+            }
+        }
+        return nums[start];
+    }
+};
